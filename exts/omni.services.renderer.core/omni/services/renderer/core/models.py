87cdabebecf9ebf70ffb76580faaefccd99501bd
Leland Fowler <lelandplusplus@gmail.com>
2023-12-05 13:41:16 -0800

    feat: Update models for new json design.

--- exts/omni.services.renderer.core/omni/services/renderer/core/models.py
+++ exts/omni.services.renderer.core/omni/services/renderer/core/models.py
@@ -1,90 +1,42 @@
 from typing import List
-from pydantic import dataclasses
+from pydantic.dataclasses import dataclass
 
-
-@dataclasses.dataclass
+@dataclass
 class Entity:
-        entity_id: str
-        prim_path: str
+    entity_id: int
+    entity_type: str
+    prim_path: str
 
-@dataclasses.dataclass
+@dataclass
 class Implant:
-    name: str
-    usd_path: str
+    entity_id: int
     prop_type: str
-    transforms: List[str]
+    dataset_path: str
+    dataset_key: str
 
-@dataclasses.dataclass
+
+@dataclass
 class RenderView:
-    camera_path: str
-    entity_id: str
+    camera_id: str
+    entity_id: int
     visibility: str
 
-class Payload_Base:
-    @classmethod
-    def load(self):
-        raise NotImplementedError
+@dataclass
+class Entities:
+    entities: List[Entity]
 
-class Entities(Payload_Base):
-    def __init__(
-            self, 
-            entities: List[Entity]
-        ) -> None:
-        self.entities = entities
+@dataclass
+class Implants:
+    implants: List[Implant]
 
-    @classmethod
-    def load(self, entities):
-        """
-        Description:
-        Load a list of entities from a JSON file.
-        Args:
-        entities (dict): List of entities to load.
-        Returns:
-        list[Entity]: List of entities loaded from the JSON file.
-        """
-        return [
-            Entity(entity_id, prim_path) 
-            for entity_id, prim_path 
-            in entities.items()
-        ]
+@dataclass
+class RenderViews:
+    views: List[RenderView]
 
-class Implants(Payload_Base):
-    def __init__(
-            self, 
-            implants: List[Implant]
-        ) -> None:
-        self.implants = implants
-    
-    @classmethod
-    def load(self, implants):
-        return [
-            Implant(name, usd_path, prop_type, transforms)
-            for name, usd_path, prop_type, transforms
-            in implants
-        ]
-
-class RenderViews(Payload_Base):
-    def __init__(
-            self, 
-            views: List[RenderView]
-        ) -> None:
-        self.views = views
-    
-    @classmethod
-    def load(self, render_views):
-        return [
-            RenderView(camera_path, entity_id, visibility)
-            for camera_path, entity_id, visibility
-            in render_views
-        ]
-
+@dataclass
 class RenderSettings:
-     def __init__(
-             self, 
-             settings: dict
-        ) -> None:
-        self.img_w: settings["img_w"]
-        self.img_h: settings["img_h"]
-        self.transparency: settings["transparency"]
-        self.color_mode: settings["color_mode"]
-        self.samples: int = settings["samples"]
+    img_w: int
+    img_h: int
+    transparency: bool
+    color_mode: str
+    samples: int
